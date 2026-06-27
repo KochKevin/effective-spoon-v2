548af1c676127973ce1b187608e39d6611978c78
KochKevin <147712148+KochKevin@users.noreply.github.com>
2026-06-27 10:58:27 +0000

    feat: basic layout

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,4 +1,16 @@
-<sc
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+</script>
 
 
 <template>
@@ -8,10 +20,33 @@
   </div>
 
 
+  <AlertDialog >
+    <AlertDialogTrigger>Open</AlertDialogTrigger>
 
+    <AlertDialogContent class="flex flex-col">
+      <AlertDialogHeader >
+        <AlertDialogTitle>Produkte</AlertDialogTitle>
+      </AlertDialogHeader>
+
+        <div class="grid grid-cols-4 gap-4 flex-8">
+            <Card>T</Card>
+            <Card>T</Card>
+            <Card>T</Card>
+            <Card>T</Card>
+            <Card>T</Card>
+      
+
+          </div>
+
+
+      <AlertDialogFooter class="">
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+
+
+  </AlertDialog>
 
 
 </template>
-
-
-
