367bac92054ecae957dc7d97cec1c5db051bdaaa
KochKevin <147712148+KochKevin@users.noreply.github.com>
2026-06-28 20:42:45 +0000

    fix: frontend component cleanup

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,26 +1,5 @@
 <script setup lang="ts">
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog'
-import { useProductsStore } from './stores/products';
 
-const productStore = useProductsStore()
-
-
-onMounted( () => {
-  productStore.fetchProducts()
-
-  console.log(productStore.isLoading)
-  console.log(productStore.products)
-})
 
 </script>
 
@@ -32,36 +11,7 @@ onMounted( () => {
   </div>
 
 
-  <AlertDialog >
-    <AlertDialogTrigger>Open</AlertDialogTrigger>
-
-    <AlertDialogContent class="flex flex-col">
-      <AlertDialogHeader >
-        <AlertDialogTitle>Produkte</AlertDialogTitle>
-      </AlertDialogHeader>
-
-       <p v-if="productStore.isLoading">Lade...</p>
-
-        <div v-else class="grid grid-cols-4 gap-4 flex-8">
-
-         
-          <Card v-for="product in productStore.products" :key="product.id">
-           <p> {{ product.name }} </p>
-          </Card>
-
-      
-
-          </div>
-
-
-      <AlertDialogFooter class="">
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction>Continue</AlertDialogAction>
-      </AlertDialogFooter>
-    </AlertDialogContent>
-
-
-  </AlertDialog>
+  <ProductCatalogModal></ProductCatalogModal>
 
 
 </template>
---
+++ frontend/app/components/productCatalogModal.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+
+const productStore = useProductsStore()
+
+
+onMounted( () => {
+  productStore.fetchProducts()
+
+  console.log(productStore.isLoading)
+  console.log(productStore.products)
+})
+
+</script>
+
+
+<template>
+ <AlertDialog >
+    <AlertDialogTrigger as-child>
+        <Button variant="outline">Produkte</Button>
+    </AlertDialogTrigger>
+
+    <AlertDialogContent class="flex flex-col">
+      <AlertDialogHeader >
+        <AlertDialogTitle>Produkte</AlertDialogTitle>
+      </AlertDialogHeader>
+
+       <p v-if="productStore.isLoading">Lade...</p>
+
+        <div v-else class="grid grid-cols-4 gap-4 flex-8">
+
+         
+          <Card v-for="product in productStore.products" :key="product.id">
+           <p> {{ product.name }} </p>
+          </Card>
+
+      
+
+          </div>
+
+
+      <AlertDialogFooter class="">
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+
+
+  </AlertDialog>
+
+
+</template>
