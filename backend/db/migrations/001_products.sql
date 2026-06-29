4689fe221a48ca5ab2d94a4b56102b041304f042
KochKevin <147712148+KochKevin@users.noreply.github.com>
2026-06-29 16:24:23 +0000

    feat: adding new test products

--- backend/db/migrations/001_products.sql
+++ backend/db/migrations/001_products.sql
@@ -8,6 +8,8 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 -- Add test Product
-INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000000', 'test', 100);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000001', 'kola', 100);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000002', 'fanta', 100);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000003', 'wasser', 70);
 
 -- +goose StatementEnd
