86fff3f004250cd795e27c7f774426420290e97d
KochKevin <147712148+KochKevin@users.noreply.github.com>
2026-07-04 12:18:26 +0000

    feat: adding basic buy and cancel buttons and in general making the ui better

--- backend/db/migrations/001_products.sql
+++ backend/db/migrations/001_products.sql
@@ -11,5 +11,8 @@ CREATE TABLE IF NOT EXISTS products (
 INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000001', 'kola', 100);
 INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000002', 'fanta', 100);
 INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000003', 'wasser', 70);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000004', 'sprite', 100);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000005', 'mezzo-mix', 100);
+INSERT INTO products (id, name, price) VALUES ('00000000-0000-0000-0000-000000000006', 'spezi', 100);
 
 -- +goose StatementEnd
--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -7,21 +7,43 @@
 <template>
 
 
-  <Card class="flex flex-row">
+  <div class="flex flex-col h-dvh p-2 gap-4">
 
-    <Card class="flex-4" />
+    <Card class="flex-none flex flex-row gap-4 h-22 p-4">
 
-    <ProductCatalogModal class="flex-2"/>
+      <Card class="flex-4" />
 
-    <Card class="flex-4" />
+      <ProductCatalogModal class="flex-2"/>
 
-  </Card>
+      <Card class="flex-4" />
 
-  
+    </Card>
 
-  <ShoppingCartGrid></ShoppingCartGrid>
+    
+
+    <ShoppingCartGrid class="flex-1 min-h-0 overflow-y-auto p-2" />
+
+    <div class="flex-none w-full border-t pt-4">
+
+      <div class="flex items-center justify-between w-full max-w-5xl mx-auto px-4">
+
+          <Button size="lg" class="w-32">
+          Abbrechen
+        </Button>
+
+          <div class="text-2xl font-bold tracking-tight">
+          TOTAL: 99,99 €
+        </div>
+
+          <Button size="lg" class="w-32">
+          Kaufen
+        </Button>
+
+        </div>
+      </div>
 
 
+  </div>
 
 
 
