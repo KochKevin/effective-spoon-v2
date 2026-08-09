e5428f26b1ca1891df7f4fb0a27a7d0d71d01d0f
Kevin <kevin@email.de>
2026-08-09 20:43:17 +0200

    feat: instead of using its won chi router, SSE will now use the generated handler and expands it. Also it is now under api/pushes reachable

--- backend/internal/push/handler.go
+++ backend/internal/push/handler.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
+	pushapi "github.com/KochKevin/effective-spoon-v2/internal/push/generated"
 	pushservice "github.com/KochKevin/effective-spoon-v2/internal/push/service"
-	"github.com/go-chi/chi/v5"
 )
 
 type PushService interface {
@@ -14,65 +14,60 @@ type PushService interface {
 }
 
 type Api struct {
+	pushapi.Unimplemented
 	PushService PushService
 }
 
 const endOfLine = "\n"
 const lineSeperator = "\n"
 
+// This system is currently limited to one connection. It would break if multiple clients would be connected. But for its current purpose its enough
+func (a *Api) GetPushes(w http.ResponseWriter, r *http.Request) {
 
-//This system is currently limited to one connection. It would break if multiple clients would be connected. But for its current purpose its enough
+	slog.Debug("GET on /üushes")
 
-func (a *Api) GetEvents(r chi.Router) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
 
-	r.Get("/events", func(w http.ResponseWriter, r *http.Request) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 
-		slog.Debug("GET on /events")
+	for {
 
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
+		//DO WORK
+		//slog.Debug("Hallo!")
 
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
+		w.Write([]byte(": heartbeat" + endOfLine + lineSeperator))
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
 
-		for {
+		select {
+		//rerun work
+		case <-ticker.C:
+			continue
 
-			//DO WORK
-			//slog.Debug("Hallo!")
+		case event := <-a.PushService.GetEventChannel():
 
-			w.Write([]byte(": heartbeat" + endOfLine + lineSeperator))
+			if event == pushservice.ShoppingCartUpdateEvent {
+
+				w.Write([]byte("data: " + event + endOfLine + lineSeperator))
+
+			} else if event == pushservice.UserLoginEvent {
+				w.Write([]byte("data: " + event + endOfLine + lineSeperator))
+			}
+
+			//Push new message
 			if f, ok := w.(http.Flusher); ok {
 				f.Flush()
 			}
+			continue
 
-			select {
-			//rerun work
-			case <-ticker.C:
-				continue
-
-			case event := <-a.PushService.GetEventChannel():
-
-				if event == pushservice.ShoppingCartUpdateEvent {
-
-					w.Write([]byte("data: " + event + endOfLine + lineSeperator))
-
-				} else if event == pushservice.UserLoginEvent {
-					w.Write([]byte("data: " + event + endOfLine + lineSeperator))
-				}
-
-				//Push new message
-				if f, ok := w.(http.Flusher); ok {
-					f.Flush()
-				}
-				continue
-
-			//End Ticker when connection is closed
-			case <-r.Context().Done():
-				return
-			}
+		//End Ticker when connection is closed
+		case <-r.Context().Done():
+			return
 		}
-
-	})
+	}
 
 }
