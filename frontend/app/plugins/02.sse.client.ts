44d36cd6f0c311f53b2041d2038cb922a923eacb
Kevin <kevin@email.de>
2026-08-10 22:55:16 +0200

    feat: adding try catch to catch errors on creating the sse connection

--- frontend/app/plugins/02.sse.client.ts
+++ frontend/app/plugins/02.sse.client.ts
@@ -5,12 +5,19 @@ import { getPushes, type SseEvent } from "~/api";
 export const sseBus = useEventBus<SseEvent>('sse-event');
 
 export default defineNuxtPlugin(async () => {
-    const {stream} = await getPushes();
+    const { stream } = await getPushes();
 
-    (async () => {
-        for await (const event of stream) {
-            sseBus.emit(event);
-        }
-    })();
+    try {
 
+        const { stream } = await getPushes();
+
+        (async () => {
+            for await (const event of stream) {
+                sseBus.emit(event);
+            }
+        })();
+
+    } catch (err) {
+        console.error("Failed to establish SSE connection:", err)
+    }
 })
